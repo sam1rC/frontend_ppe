857f823ad66e394e8cf434834a660095fcf25044
sgez14 <samir.colpas@hotmail.com>
2022-05-01 17:05:20 -0500

    se empezo a realizar el proceso de login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,2 +1,23 @@
 let btn_enviar, formulario;
-let corre,clave;
+let corre,clave,usuario;
+
+window.onload = function()
+{
+	btn_enviar = document.getElementById("btn_enviar");
+	correo = document.getElementById("correo");
+	clave = document.getElementById("clave");
+	formulario = document.getElementById("formulario");
+	formulario.addEventListener("submit", procesarLogin);
+}
+
+function procesarLogin()
+{
+	let txt_correo, txt_clave;
+	let str_usuario, md5_clave;
+	let error = false;
+	txt_correo = correo.value;
+	txt_clave = md5(clave.value);
+
+	str_usuario = localStorage.getItem("usuario");
+	usuario = JSON.parse(str_usuario)
+}
