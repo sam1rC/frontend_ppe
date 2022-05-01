216e7375964c5b82fda85e2e8951b210819744c0
sgez14 <samir.colpas@hotmail.com>
2022-05-01 15:03:27 -0500

    se agrego la funcion procesarRegistro

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,7 +1,26 @@
-let correo,clave
+let correo,clave,formulario
 
 window.onload = function()
 {
 	correo = document.getElementById("correo");
 	clave = document.getElementById("clave");
+	formulario = document.getElementById("formulario");
+	formulario.addEventListener("submit",procesarRegistro);
+}
+
+function procesarRegistro(evento){
+
+	let txt_correo,txt_clave
+	let str_usuario;
+	txt_correo = correo.value;
+	txt_clave = md5(clave.value);
+
+	usuario = {
+		correo:txt_correo,
+		clave:txt_clave,
+	};
+
+	str_usuario = JSON.stringify(usuario);
+	localStorage.setItem("usuario",str_usuario);
+
 }
